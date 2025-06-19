669719bbe80e66272659c5a94c584cbc1ffae590
shun28a <ai.76ers.i3@gmail.com>
2025-06-20 00:17:32 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontSize: '24px', color: '#4caf50' }}>
+      <h1>ビルド成功！</h1>
+      <p>ついに、ここまでたどり着きました！</p>
+    </div>
+  );
+}
+
+export default App;
